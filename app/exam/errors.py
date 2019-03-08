2fa244f1f2aed5c4ee805e669c738e26a2d2598b
dylanchu <chdy.uuid@gmail.com>
2019-03-08 18:00:27 +0800

    feat:add exam batch test functions

--- app/exam/errors.py
+++ app/exam/errors.py
@@ -9,4 +9,16 @@ def success(x=None):
     return {**d, **x}
 
 
-Add_task_failed = {'code': 4, 'msg': '', 'needDisplay': True, 'tip': '添加任务失败'}
+def error(x=None):
+    d = {'code': 1, 'msg': 'error'}
+    return {**d, **x}
+
+
+def exception(x=None):
+    d = {'code': 1, 'msg': 'exception'}
+    return {**d, **x}
+
+
+Params_error = {'code': 4000, 'msg': '请求参数错误'}
+Exam_not_exist = {'code': 4001, 'msg': '测试不存在'}
+Add_task_failed = {'code': 4101, 'msg': '', 'needDisplay': True, 'tip': '添加任务失败'}
--- app/exam/views.py
+++ app/exam/views.py
@@ -3,7 +3,93 @@
 #
 # Created by dylanchu on 19-2-25
 
-from . import exam
+from . import exam, errors
+from .config import PathConfig
 from app.models.user import User
-from flask import redirect, url_for, current_app
-from flask_login import login_required, current_user
+from app.models.exam import *
+from flask import request, current_app, jsonify
+from celery_tasks import analysis_main_12, analysis_main_3
+import datetime
+import random
+import shutil
+import time
+import os
+import traceback
+
+
+@exam.route('/upload-success-for-test', methods=['POST'])
+def upload_success_for_test():
+    upload_url = request.form.get("uploadUrl")
+    q_type = int(request.form.get("questionType"))
+    if not upload_url or not q_type:
+        return jsonify(errors.Params_error)
+    _temp_folder = "%sr%s" % (time.time(), random.randint(100, 1000))
+    temp_url = '/'.join((PathConfig.audio_copy_temp_basedir, "batch_test", _temp_folder, os.path.basename(upload_url)))
+
+    current_app.logger.info("upload_success_for_test: upload_url: " + upload_url)
+
+    # 新建一个current
+    current_test = CurrentTestModel()
+    current_test.test_start_time = datetime.datetime.utcnow()
+    q = QuestionModel.objects(q_type=q_type).order_by('used_times')[0]
+    q_current = CurrentQuestionEmbed(q_id=q.id.__str__(), q_type=q.q_type, q_text=q.text, wav_upload_url='',
+                                     wav_temp_url='')
+    current_test.questions = {"1": q_current}
+    current_test.save()
+
+    try:
+        # chdir should be 'xxx/expressiveness_server'
+        dir_name = os.path.dirname(temp_url)
+        if not os.path.exists(dir_name):
+            os.makedirs(dir_name)
+        shutil.copyfile(upload_url, temp_url)
+    except Exception as e:
+        current_app.logger.error(e)
+
+    # change question state to handling
+    current_test.questions['1'].status = 'handling'
+    current_test.questions['1'].wav_upload_url = upload_url.lstrip('/expression')
+    current_test.questions['1'].wav_temp_url = ''.join(('/expression/', temp_url))
+    current_test.questions['1']['analysis_start_time'] = datetime.datetime.utcnow()
+    current_test.save()
+
+    try:
+        if q.q_type == 3 or q.q_type == '3':
+            ret = analysis_main_3.apply_async(args=(str(current_test.id), '1'), queue='for_q_type3', priority=10)
+            # todo: store ret.id in redis for status query
+        else:
+            ret = analysis_main_12.apply_async(args=(str(current_test.id), '1'), queue='for_q_type12', priority=2)
+            # todo: store ret.id in redis for status query
+        current_app.logger.info("AsyncResult id: %s" % ret.id)
+    except Exception as e:
+        current_app.logger.error('upload_success_for_test: celery enqueue:\n%s' % traceback.format_exc())
+        return jsonify(errors.exception({'Exception': str(e)}))
+    resp = {"status": "Success", "desc": "添加任务成功，等待服务器处理", "dataID": current_test.id.__str__(), "taskID": ret.id}
+    return jsonify(errors.success(resp))
+
+
+@exam.route('/get-result-for-test', methods=['POST'])
+def get_result_for_test():
+    test_id = request.form.get('testID')
+    if not test_id or (len(test_id) != 12 and len(test_id) != 24):
+        return jsonify(errors.Params_error)
+    tests = CurrentTestModel.objects(id=test_id).first()
+    if len(tests) == 0:
+        return jsonify(errors.Exam_not_exist)
+    try:
+        question = tests['questions']['1']
+        print(tests)
+        print('------')
+        print(question)
+        status = question['status']
+        start = question['analysis_start_time']
+        end = question['analysis_end_time']
+        if status != 'error':
+            msg = {"status": status, "analysis_start_time": start, "analysis_end_time": end}
+            return jsonify(errors.success(msg))
+        else:
+            msg = {"status": status, "stack": question['stack'], "analysis_start_time": start, "analysis_end_time": end}
+        return jsonify(errors.error(msg))
+    except Exception as e:
+        current_app.logger.error('get_result_for_test: %s' % traceback.format_exc())
+        return jsonify(errors.exception({'Exception': str(e)}))
