8aad09b445713f3502c4e8b0e41b9e86c99b80fe
dylanchu <chdy.uuid@gmail.com>
2019-03-16 16:32:51 +0800

    change: redirect / to /index.html

--- app/main/views.py
+++ app/main/views.py
@@ -4,9 +4,10 @@
 # Created by dylanchu on 19-2-22
 
 from . import main
+from flask import redirect
 
 
 @main.route('/')
 @main.route('/index')
 def index():
-    return 'index page'
+    return redirect('/index.html')
