76437ec50c9e5d634e3fb5eb7aadfeb6e2f5b803
gyue15 <786330297@qq.com>
2019-03-29 18:49:32 +0800

    fix: 后台接收不到参数的bug

--- app/admin/optimize.py
+++ app/admin/optimize.py
@@ -10,40 +10,42 @@ import datetime
 
 @admin.route('/get-score-data', methods=['GET'])
 def get_score_data():
-    question_num = request.form.get('question-num')
+    question_num = request.args.get('questionNum')
     print("get_score_data: questionNum: " + str(question_num))
     return jsonify(errors.success(mock_data.score_data))
 
 
 @admin.route('/get-weight-data', methods=['GET'])
 def get_weight_data():
-    question_num = request.form.get("question-num")
+    question_num = request.args.get("questionNum")
     print("get_weight_data: questionNum: " + str(question_num))
     return jsonify(errors.success(mock_data.weight_data))
 
 
 @admin.route('/update-weight', methods=['POST'])
 def update_weight():
-    question_num = int(request.form.post('questionNum'))
-    weight = json.loads(request.form.post("weight"))
+    question_num = request.form.get('questionNum')
+    weight = json.loads(request.form.get("weight"))
 
     print("update_weight: questionNum: " + str(question_num))
     print(weight)
+    print(weight["keyWords"])
+    print(type(weight["keyWords"][0]))
 
     return jsonify(errors.success(mock_data.weight_data))
 
 
 @admin.route('/get-last-cost-data', methods=['GET'])
 def get_last_cost_data():
-    question_num = request.form.get("question-num")
+    question_num = request.args.get("questionNum")
     print("get_last_cost_data: questionNum: " + str(question_num))
     return jsonify(errors.success(mock_data.cost_data))
 
 
 @admin.route('/start-auto-optimize', methods=['POST'])
 def start_auto_optimize():
-    question_num = int(request.form.post("question-num"))
-    settings = json.loads(request.form.post("settings"))
+    question_num = request.form.get("questionNum")
+    settings = json.loads(request.form.get("settings"))
     print("start_auto_optimize: questionNum: " + str(question_num))
     print(settings)
     return jsonify(errors.success())
@@ -51,14 +53,14 @@ def start_auto_optimize():
 
 @admin.route('/stop-auto-optimize', methods=['POST'])
 def stop_auto_optimize():
-    question_num = int(request.form.post("question-num"))
+    question_num = request.form.get("questionNum")
     print("stop_auto_optimize: questionNum: " + str(question_num))
     return jsonify(errors.success())
 
 
 @admin.route('/refresh-auto-optimize', methods=['POST'])
 def refresh_auto_optimize():
-    question_num = int(request.form.post("question-num"))
+    question_num = request.form.get("questionNum")
     print("refresh_auto_optimize: questionNum: " + str(question_num))
     return jsonify(errors.success())
 
