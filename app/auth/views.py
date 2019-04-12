3a40f6d4ea93a514e470664fb67548ddf9eff8b1
zhanglei <151250193@smail.nju.edu.cn>
2019-04-12 17:08:03 +0800

    historyscores

--- app/auth/views.py
+++ app/auth/views.py
@@ -94,8 +94,8 @@ def showscore():
     check_user = UserModel.objects(email=email).first()
     user_id=check_user.id
     scorelist=CurrentTestModel.objects(user_id=user_id)
-    if scorelist is None:
-        return jsonify(errors.Exam_not_exist)
+    if scorelist.count()==0:
+        return jsonify(errors.No_history)
     return jsonify(errors.success(scorelist))
 
 
