de4a402c1403b08c6de40d4ba980b41d8869d61d
CharlesFeng <fdfengjunjie@126.com>
2019-08-10 17:08:03 +0800

    feat: 获取某用户的成绩情况支持手机号

--- app/admin/score.py
+++ app/admin/score.py
@@ -4,6 +4,7 @@ from flask import current_app, jsonify
 
 from app import errors
 from app.admin.admin_config import ScoreConfig
+from app.auth.util import validate_email
 from app.exam.exam_config import ExamConfig
 from app.models.analysis import *
 from app.models.user import UserModel
@@ -13,7 +14,7 @@ from .algorithm import OptimizeAlgorithm
 
 @admin.route('/score/question', methods=['GET'])
 def get_score_of_all_questions():
-    """获取各题目平均分的成绩情况 TODO
+    """获取各题目平均分的成绩情况
 
     :return: 所有题目，可直接展示
     """
@@ -30,7 +31,7 @@ def get_score_of_all_questions():
 
 @admin.route('/score/question/<id>', methods=['GET'])
 def get_score_of_specific_questions(id):
-    """获取某题目的成绩情况 TODO
+    """获取某题目的成绩情况
 
     :param id: 题号ID
     :return: 该问题的成绩情况
@@ -56,7 +57,7 @@ def get_score_of_specific_questions(id):
 
 @admin.route('/score/user', methods=['GET'])
 def get_score_of_all_users():
-    """获取各用户平均分的成绩情况 TODO
+    """获取各用户平均分的成绩情况
 
     :return: 所有题目，可直接展示
     """
@@ -67,36 +68,52 @@ def get_score_of_all_users():
         return jsonify(errors.Score_no_data)
 
     mapper = UserMapper()
-    result = __generate_result_from_dict(mapper.map_answers(all_answers), 'userEmail')
+    result = __generate_result_from_dict(mapper.map_answers(all_answers), 'username')
 
-    # map_answers_by_user() 返回的结果中 key 为 ObjectId，需要改为 email
+    # map_answers_by_user() 返回的结果中 key 为 ObjectId，需要改为 email / phone
     for item in result:
-        cur_user_id = item['userEmail']
+        cur_user_id = item['username']
         cur_user = UserModel.objects(id=cur_user_id).first()
-        cur_user_email = cur_user if cur_user else cur_user_id.__str__() + ScoreConfig.DEFAULT_USER_EMAIL
-        item['userEmail'] = cur_user_email
+
+        if cur_user:
+            cur_username = cur_user['email'] if cur_user['email'] else cur_user['phone']
+        else:
+            cur_username = cur_user_id.__str__() + ScoreConfig.DEFAULT_USER_EMAIL
+
+        item['username'] = cur_username
     return jsonify(errors.success({'result': result}))
 
 
-@admin.route('/score/user/<user_email>', methods=['GET'])
-def get_score_of_specific_users(user_email):
+@admin.route('/score/user/<username>', methods=['GET'])
+def get_score_of_specific_users(username):
     """获取某用户的成绩情况
 
-    :param user_email: 用户邮箱
+    :param username: 用户邮箱
     :return: 该用户的成绩情况
     """
-    current_app.logger.info('get_score_of_specific_users   ' + user_email)
+    current_app.logger.info('get_score_of_specific_users   ' + username)
 
-    if 'default@site.com' in user_email:
-        # 默认邮箱，通过主键查询
-        user_object_id = user_email[:-16]
-        all_answers = AnalysisModel.objects(user=user_object_id).order_by('date')
+    if '@' in username:
+        # 邮箱登录
+        email = username
+        if not validate_email(email):
+            return jsonify(errors.Params_error)
+        if ScoreConfig.DEFAULT_USER_EMAIL in username:
+            # 默认邮箱，通过主键查询
+            user_object_id = username[:-16]
+            all_answers = AnalysisModel.objects(user=user_object_id).order_by('date')
+        else:
+            # 真实邮箱
+            cur_user = UserModel.objects(email=username).first()
+            if not cur_user:
+                return jsonify(errors.User_not_exist)
+            all_answers = AnalysisModel.objects(user=cur_user['id']).order_by('date')
     else:
-        cur_user = UserModel.objects(email=user_email).first()
+        # 手机号登录
+        cur_user = UserModel.objects(phone=username).first()
         if not cur_user:
             return jsonify(errors.User_not_exist)
         all_answers = AnalysisModel.objects(user=cur_user['id']).order_by('date')
-
 
     if len(all_answers) == 0:
         return jsonify(errors.Score_no_data)
