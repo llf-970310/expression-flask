1e2607f983755ff5c2e82ba16f4d7fff7e129e0e
dylanchu <chdy.uuid@gmail.com>
2019-11-23 00:47:22 +0800

    chore:删除无用的main蓝图

--- app/__init__.py
+++ app/__init__.py
@@ -48,8 +48,6 @@ def create_app():
 
     login_manager.init_app(app)
 
-    from .main import main as main_blueprint
-    app.register_blueprint(main_blueprint, static_folder='static')
     from .auth import auth as auth_blueprint
     app.register_blueprint(auth_blueprint, url_prefix='/api/auth')
     from .exam import exam as exam_blueprint
