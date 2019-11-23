4304e9e978584abc66391560513789b9bbe14327
dylanchu <chdy.uuid@gmail.com>
2019-11-23 15:46:21 +0800

    feat: @admin_login_required

--- app/auth/util.py
+++ app/auth/util.py
@@ -3,8 +3,23 @@
 # Author     : tangdaye
 # Description: auth util
 
+from functools import wraps
+from flask import jsonify
+from flask_login import login_required, current_user
+from app import errors
+
 
 def validate_email(email):
     import re
     p = re.compile(r"[^@]+@[^@]+\.[^@]+")
     return p.match(email)
+
+
+def admin_login_required(func):
+    @login_required
+    @wraps(func)
+    def decorated_view(*args, **kwargs):
+        if not current_user.is_admin():
+            return jsonify(errors.Admin_login_required)
+        return func(*args, **kwargs)
+    return decorated_view
