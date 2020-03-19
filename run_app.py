4924c85402e35774f827971e7ef7a4e19a86ea96
dylanchu <chudongyu@126.com>
2020-03-20 02:08:22 +0800

    adjust banner

--- run_app.py
+++ run_app.py
@@ -4,7 +4,6 @@
 # Created by dylanchu on 19-2-22
 
 from app import create_app
-import logging
 
 banner = r'''
   _____  ___ __  _ __ ___  ___ ___(_) ___  _ __
@@ -12,11 +11,11 @@ banner = r'''
 |  __/>  <| |_) | | |  __/\__ \__ \ | (_) | | | |
  \___/_/\_\ .__/|_|  \___||___/___/_|\___/|_| |_|
           |_|
+
 '''
-print(banner)
-print('================================================')
-print('             CREATING   FLASK  APP              ')
-print('================================================')
+print('=================================================')
+print('             CREATING   FLASK  APP               ')
+print('=================================================')
 
 app = create_app()
 
@@ -24,8 +23,10 @@ print('==================================================')
 print('                FLASK APP CREATED                 ')
 print('                  STARTING  NOW                   ')
 print('==================================================')
+print(banner)
 
 if __name__ == '__main__':
+    import logging
     # handler = logging.FileHandler('flask.log')
     # app.logger.addHandler(handler)
     # print(app.logger.handlers)
