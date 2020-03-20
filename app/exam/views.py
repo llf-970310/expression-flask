dc6057ad27ff74095589576df10511eb355c54ca
dylanchu <chudongyu@126.com>
2020-03-21 03:21:57 +0800

    feat:exam views: GET upload-url v2

--- app/exam/views.py
+++ app/exam/views.py
@@ -135,6 +135,53 @@ def get_pretest_result_v2():
         return jsonify(errors.success({"canRcg": False, "qualityIsOk": False}))
 
 
+@exam.route('/<question_num>/upload-url', methods=['GET'])
+def get_upload_url_v2(question_num):
+    test_id = ExamSession.get(current_user.id, "test_id",
+                              default=DefaultValue.test_id)  # for production
+    # get test
+    current_test = CurrentTestModel.objects(id=test_id).first()
+    if current_test is None:
+        current_app.logger.error("[TestNotFound][get_upload_url_v2]test_id: %s" % test_id)
+        return jsonify(errors.Exam_not_exist)
+
+    # get question
+    user_id = str(current_user.id)
+    current_app.logger.debug("[DEBUG][get_upload_url_v2]question_num: %s, user_name: %s"
+                             % (question_num, current_user.name))
+    try:
+        question = current_test.questions[question_num]
+    except Exception as e:
+        current_app.logger.error("[GetEmbeddedQuestionException][get_upload_url_v2]question_num: "
+                                 "%s, user_name: %s. exception:\n%s"
+                                 % (question_num, current_user.name, e))
+        return jsonify(errors.Get_question_failed)
+
+    # sts = auth.get_sts_from_redis()  # a production to--do
+
+    """generate file path
+    upload file path: 相对目录(audio)/日期/用户id/时间戳+后缀(.wav)
+    temp path for copy: 相对目录(temp_audio)/用户id/文件名(同上)
+    """
+
+    # 如果数据库没有该题的url，创建url，存数据库 ，然后返回
+    # 如果数据库里已经有这题的url，说明是重复请求，不用再创建
+    if not question.wav_upload_url:
+        file_dir = '/'.join((PathConfig.audio_save_basedir, get_server_date_str('-'), user_id))
+        _temp_str = "%sr%s" % (int(time.time()), random.randint(100, 1000))
+        file_name = "%s%s" % (_temp_str, PathConfig.audio_extension)
+        question.wav_upload_url = file_dir + '/' + file_name
+        question.file_location = 'BOS'
+        question.status = 'url_fetched'
+        current_test.save()
+        current_app.logger.info("[NewUploadUrl][get_upload_url]user_id:%s, url:%s"
+                                % (current_user.id, question.wav_upload_url))
+        current_app.logger.info("[INFO][get_upload_url_v2]new url: " + question.wav_upload_url)
+
+    context = {"fileLocation": "BOS", "url": question.wav_upload_url}
+    return jsonify(errors.success(context))
+
+
 @exam.route('/get-upload-url', methods=['POST'])
 @login_required
 def get_upload_url():
@@ -150,7 +197,8 @@ def get_upload_url():
     # get question
     user_id = str(current_user.id)
     # 这里不能用session存储题号，由于是异步处理，因此session记录到下一题的时候上一题可能还没处理完
-    current_app.logger.info("get_upload_url: question_num: %s, user_name: %s" % (str(question_num), current_user.name))
+    current_app.logger.info("get_upload_url: question_num: %s, user_name: %s"
+                            % (str(question_num), current_user.name))
     question = current_test.questions[str(question_num)]
 
     """generate file path
