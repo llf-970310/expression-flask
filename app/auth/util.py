a1c09a85ad95699ede3f67faac27b5a1aa98873e
Longfei Li <lf97310@gmail.com>
2020-07-11 13:15:46 +0800

    fix: valid phone

--- app/auth/util.py
+++ app/auth/util.py
@@ -19,7 +19,7 @@ def validate_email(email):
 
 def validate_phone(phone):
     import re
-    p = re.compile(r"^(13[0-9]|14[5|7]|15[0|1|2|3|4|5|6|7|8|9]|18[0|1|2|3|5|6|7|8|9])\d{8}$")
+    p = re.compile(r"^1[3-9]\d{9}$")
     return p.match(phone)
 
 
